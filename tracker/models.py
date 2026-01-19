57ce5bfe5049fd0c3eb426f043521c0fee7ef331
Raguru Narendra Kumar Reddy <110231342+NarendraKumarReddy143@users.noreply.github.com>
2026-01-19 11:23:41 +0530

    Update models.py

--- tracker/models.py
+++ tracker/models.py
@@ -1,19 +1,25 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Task(models.Model):
     use_case_name = models.CharField(max_length=100)
     uce_name = models.CharField(max_length=100)
-
     tower = models.CharField(max_length=100)
-    task_type = models.CharField(max_length=100)
 
-    time_to_complete = models.DecimalField(
-        max_digits=5,
-        decimal_places=2,
-        help_text="Hours"
+    TASK_TYPE_CHOICES = [
+        ('UNIFY_UCE', 'Unify + UCE'),
+        ('ADV_UCE', 'Advance UCE'),
+        ('SERVERLESS', 'Serverless'),
+    ]
+
+    task_type = models.CharField(
+        max_length=50,
+        choices=TASK_TYPE_CHOICES
+    )
+
+    task_description = models.TextField(blank=True)
+
+    time_to_complete = models.IntegerField(
+        help_text="Minutes"
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
